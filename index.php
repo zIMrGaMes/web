a16bfb36cce86617b6383180b1ac35396deae5b1
zIMrGaMes <73715017+zIMrGaMes@users.noreply.github.com>
2022-03-02 17:48:51 +0100

    zx<d

--- index.php
+++ index.php
@@ -53,7 +53,7 @@
                     <a href="#home">Home</a>
                 </li>
                 <li>
-                    <a href="#about">About</a>
+                    <a href="about">About</a>
                 </li>
                 <li>
                     <a href="#por_que_elegirnos">Por que elegirnos</a>
@@ -285,7 +285,7 @@
                                 </div>
                               </div>
                               <div class="full testimonial_cont text_align_center">
-                                <p><strong>Eric Garcia</strong><br><strong class="ornage_color">Alquileres</strong><br><i>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit ess</i></p>
+                                <p><strong>Due markes</strong><br><strong class="ornage_color">Rental</strong><br><i>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit ess</i></p>
                                 <div class="full text_align_center margin_top_30">
                                    <img src="images/testimonial_qoute.png">
                                 </div>
