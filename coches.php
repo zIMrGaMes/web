cca37dbd6287257000d3815a3f017df4f183e311
zIMrGaMes <73715017+zIMrGaMes@users.noreply.github.com>
2022-03-12 17:02:45 +0100

    anashe

--- coches.php
+++ coches.php
@@ -173,6 +173,31 @@
                     </div>
                  </div>
               </div>
+              <div class="col-lg-4">
+                 <div class="full">
+                    <div class="choose_blog text_align_center">
+                    <img src="
+                        <?php
+                                       $sql = "SELECT * FROM elegirnos WHERE id = 2";
+                                       foreach ($pdo->query($sql) as $row) {
+                                          echo $row['imagen'];
+                                       }
+                                    ?>" />
+                        <h4>                        <?php
+                                       $sql = "SELECT * FROM elegirnos WHERE id = 2";
+                                       foreach ($pdo->query($sql) as $row) {
+                                          echo $row['tipo'];
+                                       }
+                                    ?></h4>
+                        <p>                        <?php
+                                       $sql = "SELECT * FROM elegirnos WHERE id = 2";
+                                       foreach ($pdo->query($sql) as $row) {
+                                          echo $row['texto'];
+                                       }
+                                    ?></p>
+                    </div>
+                 </div>
+              </div>
             </div>
         </div>
       </section>
