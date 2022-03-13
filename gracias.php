f3b9a21043db8fa59496c6b102e76b22c02ba08b
zIMrGaMes <73715017+zIMrGaMes@users.noreply.github.com>
2022-03-13 15:24:04 +0100

    gergerg

--- gracias.php
+++ gracias.php
@@ -68,45 +68,7 @@
                               </div>
                             </div> 
                         </div>
-
-                        <div class="carousel-item">
-
-                           <div class="testomonial_section">
-                              <div class="full center">
-                                <div class="client_img">
-                                  <img src="images/testimonial.png" alt="#" />
-                                </div>
-                              </div>
-                              <div class="full testimonial_cont text_align_center">
-                                <p><strong>Due markes</strong><br><strong class="ornage_color">Rental</strong><br><i>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit ess</i></p>
-                                <div class="full text_align_center margin_top_30">
-                                   <img src="images/testimonial_qoute.png">
-                                </div>
-                              </div>
-                            </div>  
-
-                        </div>
-
-                        <div class="carousel-item">
-
-                           <div class="testomonial_section">
-                              <div class="full center">
-                                <div class="client_img">
-                                  <img src="images/testimonial.png" alt="#" />
-                                </div>
-                              </div>
-                              <div class="full testimonial_cont text_align_center">
-                                <p><strong>Due markes</strong><br><strong class="ornage_color">Rental</strong><br><i>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit ess</i></p>
-                                <div class="full text_align_center margin_top_30">
-                                   <img src="images/testimonial_qoute.png">
-                                </div>
-                              </div>
-                            </div>  
-
-                        </div>
-
                      </div>
-                    
                   </div>
                 </div>
               </div>
